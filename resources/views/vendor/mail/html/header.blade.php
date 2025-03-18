53a5261dde12a4235ebfbd63954131e76d5989c3
D4nishD4nial <23FTT1193@student.pb.edu.bn>
2025-03-18 19:37:10 +0800

    adding logo

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,15 @@
+ <img src="{{ asset('img/pblogo.png') }}" alt="Laravel Logo" class="w-20 h-20 mb-6">@props(['url'])
+
+<tr>
+    <table class="header" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+        <tr>
+            <td class="align-center">
+                <a href="{{ config('app.url') }}" style="display: inline-block;">
+                    <img src="{{ asset('img/pblogo.png') }}" alt="Laravel Logo" class="w-20 h-20 mb-6">
+                </a>
+            </td>
+        </tr>
+    </table>
+</tr>
+
+
