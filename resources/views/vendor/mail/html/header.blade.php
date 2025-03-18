bbba01b08016b6aa7b1d0bd0b8b925fff417cb5b
D4nishD4nial <23FTT1193@student.pb.edu.bn>
2025-03-18 20:03:53 +0800

    last try

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
         <tr>
             <td class="align-center">
                 <a href="{{ config('app.url') }}" style="display: inline-block;">
-                    <img src="{{ asset('img/pblogo.png') }}" alt="Laravel Logo" class="w-20 h-20 mb-6">
+                <img src="{{ asset('pblogo.png') }}" alt="Laravel Logo" class="w-20 h-20 mb-6">
                 </a>
             </td>
         </tr>
